47e37955f5acd5fa44e756047b2d3380786fd3da
mayur <makamble8928@gmail.com>
2024-01-01 08:00:47 +0530

    Initial Commit

--- app/src/main/java/com/mayur/musicplayer/AboutActivity.kt
+++ app/src/main/java/com/mayur/musicplayer/AboutActivity.kt
@@ -10,7 +10,7 @@ class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.coolPink)
+        setTheme(MainActivity.currentThemeNav[MainActivity.themeIndex])
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.title = "About"
